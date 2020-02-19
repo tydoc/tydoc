a1aad3951779fce55406c5ee1d1d13906d01bad7
Jason Kuhrt <jasonkuhrt@me.com>
2020-02-19 17:10:25 -0500

    tests: cover alias number typeof case

--- test/extract/alias.spec.ts
+++ test/extract/alias.spec.ts
@@ -61,7 +61,7 @@ it('raw is based on type alias declaration node', () => {
   `)
 })
 
-it('type alias of number is added to type index', () => {
+it('exported type alias of number is added to type index', () => {
   expect(
     ctx.extract(`
       export type A = 1
@@ -108,4 +108,67 @@ it('type alias of number is added to type index', () => {
       },
     }
   `)
+})
+
+it('exported type alias of number via typeof is added to type index', () => {
+  expect(
+    ctx.extract(`
+      const a = 1
+      export type A = typeof a
+    `)
+  ).toMatchInlineSnapshot(`
+Object {
+  "modules": Array [
+    Object {
+      "kind": "module",
+      "location": Object {
+        "absoluteFilePath": "/a.ts",
+      },
+      "mainExport": null,
+      "name": "a",
+      "namedExports": Array [
+        Object {
+          "isTerm": false,
+          "isType": true,
+          "kind": "export",
+          "name": "A",
+          "type": Object {
+            "kind": "typeIndexRef",
+            "link": "(\\"/a\\").A",
+          },
+        },
+      ],
+    },
+  ],
+  "typeIndex": Object {
+    "(\\"/a\\").A": Object {
+      "kind": "alias",
+      "name": "A",
+      "raw": Object {
+        "nodeFullText": "",
+        "nodeText": "",
+        "typeText": "1",
+      },
+      "type": Object {
+        "base": "number",
+        "kind": "literal",
+        "name": "1",
+      },
+    },
+  },
+}
+`)
+})
+
+it('exported type alias of number acts same as if via typeof', () => {
+  expect(
+    ctx.extract(`
+      const a = 1
+      export type A = typeof a
+    `)
+  ).toEqual(
+    ctx.extract(`
+      export type A = 1
+    `)
+  )
 })
