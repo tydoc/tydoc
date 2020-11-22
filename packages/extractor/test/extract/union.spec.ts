d4e7754f76251fcfb3260932cbf8ce75a44c2ea3
Jason Kuhrt <jasonkuhrt@me.com>
2020-11-22 13:10:11 -0500

    fix(extractor): DUT no props w/ type overlaps across members
    closes #880

--- packages/extractor/test/extract/union.spec.ts
+++ packages/extractor/test/extract/union.spec.ts
@@ -1,15 +1,63 @@
 it('union types are extracted', () => {
   expect(
-    ctx.extract(`
+    ctx.extract(/*ts*/ `
       export type A = 1 | 2
     `)
   ).toMatchSnapshot()
 })
 
+it('if union members share property but they are not all literal then not considered discriminant', () => {
+  expect(
+    ctx.extract(/*ts*/ `
+        export type A = { foo: RegExp } | { foo: string[] }
+      `)
+  ).toMatchObject({
+    typeIndex: {
+      '(a).A': {
+        type: {
+          isDiscriminated: false,
+        },
+      },
+    },
+  })
+})
+
+it('if union members share property but one is not literal then not considered discriminant', () => {
+  expect(
+    ctx.extract(/*ts*/ `
+      export type A = { foo: 'bar' } | { foo: string[] }
+    `)
+  ).toMatchObject({
+    typeIndex: {
+      '(a).A': {
+        type: {
+          isDiscriminated: false,
+        },
+      },
+    },
+  })
+})
+
+it('if union members share literal type property but they overlap then not considered discriminant', () => {
+  expect(
+    ctx.extract(/*ts*/ `
+      export type A = { foo: 'bar' } | { foo: 'bar' }
+    `)
+  ).toMatchObject({
+    typeIndex: {
+      '(a).A': {
+        type: {
+          isDiscriminated: false,
+        },
+      },
+    },
+  })
+})
+
 describe('if union members share a common property name then union is considered a discriminant union', () => {
   it('alias object', () => {
     expect(
-      ctx.extract(`
+      ctx.extract(/*ts*/ `
         export type A = B | C
         type B = { b: 2; kind: 'B' }
         type C = { c: 3; kind: 'C' }
@@ -19,7 +67,7 @@ describe('if union members share a common property name then union is considered
 
   it('interface', () => {
     expect(
-      ctx.extract(`
+      ctx.extract(/*ts*/ `
         export type A = B | C
         interface B { b: 2; kind: 'B' }
         interface C { c: 3; kind: 'C' }
@@ -31,7 +79,7 @@ describe('if union members share a common property name then union is considered
 describe('if union members share multiple common properties then all are captured as discriminants', () => {
   it('alias object', () => {
     expect(
-      ctx.extract(`
+      ctx.extract(/*ts*/ `
         export type A = B | C
         type B = { b: 2; kind1: 'B1'; kind2: 'B2' }
         type C = { c: 3; kind1: 'C1'; kind2: 'C2' }
@@ -41,7 +89,7 @@ describe('if union members share multiple common properties then all are capture
 
   it('interface', () => {
     expect(
-      ctx.extract(`
+      ctx.extract(/*ts*/ `
         export type A = B | C
         interface B { b: 2; kind1: 'B1'; kind2: 'B2' }
         interface C { c: 3; kind1: 'C1'; kind2: 'C2' }
