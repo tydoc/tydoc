6aed6871d311e522825db1be5c882fb0feb0d838
Johannes Schickling <schickling.j@gmail.com>
2020-11-28 14:57:31 +0100

    Landing page list

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { FC } from 'react'
 import { Layout } from 'web/components/Layout'
 
 export default function Home() {
-  return <Layout>TODO</Layout>
+  return (
+    <Layout>
+      <ul>
+        <ListEl link="/npm/types/graphql-request" name="graphql-request" />
+        <ListEl link="/npm/src_index/swrv" name="swrv" />
+      </ul>
+    </Layout>
+  )
 }
+
+const ListEl: FC<{ link: string; name: string }> = ({ link, name }) => (
+  <li>
+    <Link href={link}>
+      <a className="">{name}</a>
+    </Link>
+  </li>
+)
