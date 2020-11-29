64ff4e942999fef960f05a570067554c668f07d8
William Luke <william@ordino.ai>
2020-11-29 08:51:18 +0300

    remove the need for fixtures

--- packages/extractor/test/__packages__/fp-ts/lib/Functor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFunctorComposition = void 0;
-function getFunctorComposition(F, G) {
-    return {
-        map: function (fa, f) { return F.map(fa, function (ga) { return G.map(ga, f); }); }
-    };
-}
-exports.getFunctorComposition = getFunctorComposition;
--- packages/extractor/test/packages.test.ts
+++ packages/extractor/test/packages.test.ts
@@ -1,17 +1,33 @@
+
+/**
+ * @jest-environment node
+ */
 import * as fs from "fs";
 import * as path from "path";
 import * as TyDoc from "../src"
 import * as tsm from 'ts-morph'
+import axios from 'axios';
+import * as tempy from 'tempy';
+import * as decompress from 'decompress';
 
-const PACKAGES_DIR = path.join(__dirname, "__packages__");
-
+const packages = [
+  "sponsorsme"
+]
 describe("tests extraction of packages", () => {
-  const packages = fs.readdirSync(PACKAGES_DIR);
-  for (const packageDirName of packages) {
-    const projectDir = path.join(PACKAGES_DIR, packageDirName)
-    test(packageDirName, async () => {
+  for (const pkg of packages) {
+    test(pkg, async () => {
+      const response = await axios.get('https://registry.npmjs.org/'+pkg)
+      const data = response.data
+      const latestVersion = data["dist-tags"]["latest"]
+      const latestData = data["versions"][latestVersion]
+      const tarUrl = latestData["dist"]["tarball"]
+      const tarPath = await downloadTarball(tarUrl)
+      const outputDir = tempy.directory()
+      await extract(tarPath, outputDir)
       expect.assertions(1)
       const tsProject = new tsm.Project()
+      const projectDir = path.join(outputDir, 'package')
+      console.log({projectDir});
       try {
         const docs = TyDoc.fromProject({
           readSettingsFromJSON: false,
@@ -27,3 +43,30 @@ describe("tests extraction of packages", () => {
     })
   }
 })
+
+async function extract(path: string, outputDir: string){
+  return new Promise<string>((resolve, reject) => {
+    decompress(path, outputDir).then((files: any) => {
+      console.log('done!');
+      resolve(outputDir)
+    }).catch(reject)
+  })
+}
+
+async function downloadTarball (url: string): Promise<string> {  
+  const path = tempy.file()
+  const writer = fs.createWriteStream(path)
+
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream'
+  })
+
+  response.data.pipe(writer)
+
+  return new Promise<string>((resolve, reject) => {
+    writer.on('finish', () => resolve(path))
+    writer.on('error', reject)
+  })
+}
