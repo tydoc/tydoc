f50d53205aea5548819d3f58e83c15b18471bf7a
Johannes Schickling <schickling.j@gmail.com>
2020-12-13 19:40:10 +0100

    feat(web): Implemented first version of url parser

--- packages/web/utils/url-parser.spec.ts
+++ packages/web/utils/url-parser.spec.ts
@@ -1,8 +1,94 @@
-import { parseUrl } from './url-parser'
+import { parseUrl, ParsedPath } from './url-parser'
 
 describe('url-parser', () => {
-  it('should work', () => {
-    const result = parseUrl('test')
-    expect(result).toEqual('test')
+  it('should parse /npm/setset', () => {
+    const result = parseUrl('/npm/setset')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: null,
+      package: 'setset',
+      version: null,
+      tag: null,
+      module: null,
+    })
+  })
+
+  it('should parse /npm/setset/v/1.0.0', () => {
+    const result = parseUrl('/npm/setset/v/1.0.0')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: null,
+      package: 'setset',
+      version: '1.0.0',
+      tag: null,
+      module: null,
+    })
+  })
+
+  it('should parse /npm/setset/t/dev', () => {
+    const result = parseUrl('/npm/setset/t/dev')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: null,
+      package: 'setset',
+      version: null,
+      tag: 'dev',
+      module: null,
+    })
+  })
+
+  it('should parse /npm/@prisma/client', () => {
+    const result = parseUrl('/npm/@prisma/client')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: '@prisma',
+      package: 'client',
+      version: null,
+      tag: null,
+      module: null,
+    })
+  })
+
+  it('should parse /npm/@prisma/client/v/1.0.0', () => {
+    const result = parseUrl('/npm/@prisma/client/v/1.0.0')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: '@prisma',
+      package: 'client',
+      version: '1.0.0',
+      tag: null,
+      module: null,
+    })
+  })
+
+  it('should parse /npm/@prisma/client/t/dev', () => {
+    const result = parseUrl('/npm/@prisma/client/t/dev')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: '@prisma',
+      package: 'client',
+      version: null,
+      tag: 'dev',
+      module: null,
+    })
+  })
+
+  it('should parse /npm/@effect-ts/core/t/dev/m/Classic/Either', () => {
+    const result = parseUrl('/npm/@effect-ts/core/t/dev/m/Classic/Either')
+    expect(result.error).toBeUndefined()
+    expect(result.value).toEqual<ParsedPath>({
+      source: 'npm',
+      org: '@effect-ts',
+      package: 'core',
+      version: null,
+      tag: 'dev',
+      module: 'Classic/Either',
+    })
   })
 })
--- packages/web/utils/url-parser.ts
+++ packages/web/utils/url-parser.ts
@@ -1,3 +1,45 @@
-export function parseUrl(url: string): string {
-  return url
+export type ParsedPath = {
+  source: 'npm'
+  org: string | null
+  package: string
+  module: string | null
+} & XOR<
+  XOR<{ version: string; tag: null }, { version: null; tag: string }>,
+  { version: null; tag: null }
+>
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never }
+type XOR<T, U> = T | U extends object
+  ? (Without<T, U> & U) | (Without<U, T> & T)
+  : T | U
+
+type Result<Err extends Error, Value> = XOR<{ error: Err }, { value: Value }>
+
+export function parseUrl(url: string): Result<ParseUrlError, ParsedPath> {
+  type MatchGroup = {
+    source: string
+    org: string | undefined
+    package: string
+    version: string | undefined
+    tag: string | undefined
+    module: string | undefined
+  }
+  // const keywords = [/** module */ 'm', /** version */ 'v', /** tag */ 't']
+  const regex = /^\/(?<source>npm)(\/(?<org>@[^\/]+))?\/(?<package>[^\/]+)((\/v\/(?<version>[^\/]+))|(\/t\/(?<tag>[^\/]+)))?(\/m\/(?<module>.+)((\/v\/)|))?$/
+  const matchResults = url.match(regex)
+  const groups = matchResults!.groups as MatchGroup
+  const resultValue = {
+    source: groups.source as 'npm',
+    org: groups.org ?? null,
+    package: groups.package,
+    tag: groups.tag ?? null,
+    version: groups.version ?? null,
+    module: groups.module ?? null,
+  } as ParsedPath
+
+  return { value: resultValue }
 }
+
+export type ParseUrlError = InvalidPathError
+
+export class InvalidPathError extends Error {}
