1d71a2ddc8a0fec52f4253febdf51ddbdf3473e4
Jason Kuhrt <jasonkuhrt@me.com>
2020-12-15 16:59:02 -0500

    chore(extractor): tweak playground again

--- packages/extractor/quokka/playground.ts
+++ packages/extractor/quokka/playground.ts
@@ -1,42 +1,16 @@
 import * as tsm from 'ts-morph'
 
-const project = new tsm.Project({
-  skipAddingFilesFromTsConfig: true,
-  useInMemoryFileSystem: true,
-  skipLoadingLibFiles: true,
-})
-const sourceFile = project.createSourceFile(
-  'playground.ts',
-  /* ts */ `
-    interface foo {
-      bar: string
-    }
-  `
-)
+const exps = new tsm.Project({ useInMemoryFileSystem: true })
+  .createSourceFile(
+    'example.ts',
+    `
+      export type Generic<T> = {}
+      export const foo: Generic<string> = {}
+      export const bar: string = ''
+    `
+  )
+  .getExportedDeclarations()
 
-const nodes = sourceFile.getChildren()
-const node = nodes[0]!
-
-node.getText() //?
-// node.getKindName() //?
-// node.getLeadingCommentRanges().map((cr) => cr.getText())[0] //?
-// node.getChildren().length //?
-// node.getChildren()[0]?.getKindName() //?
-// node.getChildren()[1]?.getKindName() //?
-
-// sourceFile.getChildren().forEach(n => {
-//   n.getKindName() //?
-//   n.getText() //?
-//   n.getFullText() //?
-//   n.getLeadingCommentRanges().map(cr => {
-//     cr.getKind() //?
-//     cr.getText() //?
-//     cr.compilerObject.pos //?
-//     cr.getEnd() //?
-//     cr.getPos() //?
-//     cr.getPos() //?
-//   }) //?
-//   if (n.getKindName() === 'EndOfFileToken') {
-//     n.getFullText() //?
-//   }
-// })
+exps.get('Generic')[0].getType().getTargetType() //?
+exps.get('foo')[0].getType().getTargetType() === exps.get('Generic')[0].getType() //?
+exps.get('bar')[0].getType().getTargetType() === undefined //?
